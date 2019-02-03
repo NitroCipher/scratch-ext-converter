64b5ee6001976890a700adff3364c599a847ca4d
James Madden <jamesbmadden@gmail.com>
2019-02-02 16:41:36 -0800

    Fix for extensions with spaces in name

--- converter.js
+++ converter.js
@@ -1,18 +1,19 @@
 export const convert = url => {
   return new Promise((resolve, reject) => {
     fetch(url).then(response => response.text()).then(data => { // Fetch the URL and get the text content
-      let name, descriptor, ext;
+      let name, descriptor, ext, id;
       const ScratchExtensions = { // Get the properties of the extension
         register: (_name, _descriptor, _ext) => {
           name = _name;
           descriptor = _descriptor;
           ext = _ext;
+          id = _name.replace(' ', '')
         }
       }
       eval(data); // Run the extension code
       let info = {
-        id: name, // Set the id to the extension's name
-        name: name,
+        id, // Set the id to the extension's name without spaces
+        name, // Set the display name
         blocks: descriptor.blocks.map((block, index) => { // convert the block to the new format
           return {
             opcode: block[2],
@@ -22,12 +23,12 @@ export const convert = url => {
         })
       };
       /* Create a String with the code for the Scratch 3 extension */
-      let result = `class ${name} {
+      let result = `class ${id} {
         getInfo() {
           return ${JSON.stringify(info)};
         }
       }
-      Scratch.extensions.register(new ${name}());`; // TODO: Add functions
+      Scratch.extensions.register(new ${id}());`; // TODO: Add functions
       resolve(result);
     });
   });
